f8f8957a2d165f4556db8474575d8f449d6ac9d5
Jose Carlos Ramos Carmenates <jramoscarmenates@gmail.com>
2015-06-24 16:52:34 -0400

    Fixed:
    - Cuando se le pasaba un objeto se hizo que si el objeto tenia id que salvara el id del objeto.
    - Se adiciono en el update  una condicion para comparar valor nuevo con valor viejo != de esta forma son comparado los dos valores y no los salva si son iguales. OJO, para este tipo de comparaciones no se puede usar !== pues este como que busca si apuntan a la misma direccion de memoria o si son del mismo tipo , esto me queda como investigacio n pendiente.

--- Listener/HistoryLogListener.php
+++ Listener/HistoryLogListener.php
@@ -102,8 +102,16 @@ class HistoryLogListener implements EventSubscriber
                         case 'create':
                             if ($entity->$method() !== null) {
                                 $old_value = '';
-                                $new_value = $entity->$method();
-                                $section   = $args->getEntity()->getId();
+                                if (( $entity->$method() instanceof \DateTime )) {
+                                    $new_value = $entity->$method()->format( 'Y-m-d H:i:s' );
+                                } elseif (is_object( $entity->$method() ) && method_exists( $entity->$method(), 'getId' )) {
+                                    $new_value = $entity->$method()->getId();
+                                } elseif (is_string( $entity->$method() ) || is_numeric( $entity->$method() )) {
+                                    $new_value = $entity->$method();
+                                } else {
+                                    goto CONTINUE_GOTO;
+                                }
+                                $section = $args->getEntity()->getId();
                             } else {
                                 goto CONTINUE_GOTO;
                             }
@@ -114,14 +122,46 @@ class HistoryLogListener implements EventSubscriber
                             if ($eventArgs->hasChangedField( $camelized_field_name )) {
                                 $old_value = $args->getOldValue( $camelized_field_name );
                                 $new_value = $args->getNewValue( $camelized_field_name );
-                                $section   = $args->getEntity()->getId();
+                                if ($old_value != $new_value) {
+                                    if ( $old_value instanceof \DateTime ) {
+                                        $old_value = $old_value->format( 'Y-m-d H:i:s' );
+                                    } elseif (is_object( $old_value ) && method_exists( $old_value, 'getId' )) {
+                                        $old_value = $old_value->getId();
+                                    } elseif (is_string( $old_value ) || is_numeric( $old_value )) {
+                                        $old_value = $old_value;
+                                    } else {
+                                        goto CONTINUE_GOTO;
+                                    }
+
+                                    if ( $new_value instanceof \DateTime ) {
+                                        $new_value = $new_value->format( 'Y-m-d H:i:s' );
+                                    } elseif (is_object( $new_value ) && method_exists( $new_value, 'getId' )) {
+                                        $new_value = $new_value->getId();
+                                    } elseif (is_string( $new_value ) || is_numeric( $new_value )) {
+                                        $new_value = $new_value;
+                                    } else {
+                                        goto CONTINUE_GOTO;
+                                    }
+
+                                    $section = $args->getEntity()->getId();
+                                }else{
+                                    goto CONTINUE_GOTO;
+                                }
                             } else {
                                 goto CONTINUE_GOTO;
                             }
                             break;
                         case 'remove':
                             if ($entity->$method() !== null) {
-                                $old_value = $entity->$method();
+                                if (( $entity->$method() instanceof \DateTime )) {
+                                    $old_value = $entity->$method()->format( 'Y-m-d H:i:s' );
+                                } elseif (is_object( $entity->$method() ) && method_exists( $entity->$method(), 'getId' )) {
+                                    $old_value = $entity->$method()->getId();
+                                } elseif (is_string( $entity->$method() ) || is_numeric( $entity->$method() )) {
+                                    $old_value = $entity->$method();
+                                } else {
+                                    goto CONTINUE_GOTO;
+                                }
                                 $new_value = '';
                                 $section   = $args->getEntity()->getId();
                             } else {
@@ -139,12 +179,8 @@ class HistoryLogListener implements EventSubscriber
                         'username'       => $this->_getUser() !== null ? $this->_getUser()->getUsername() : 'anonymous',
                         'user_ip_source' => $this->_getRequest() !== null ? $this->_getRequest()->getClientIp() : 'unknown',
                         'field_name'     => $field_name,
-                        'old_value'      => ( $old_value instanceof \DateTime ) ? $old_value->format(
-                            'Y-m-d H:i:s'
-                        ) : $old_value,
-                        'new_value'      => ( $new_value instanceof \DateTime ) ? $new_value->format(
-                            'Y-m-d H:i:s'
-                        ) : $new_value,
+                        'old_value'      => $old_value,
+                        'new_value'      => $new_value,
                         'msg'            => 'YYY',
                     ];
                     $new_cf_history_log = new CfHistoryLog( $history_log_args );
